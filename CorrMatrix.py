4139d7e601eae58f1694bbce0cc34bd109c24f97
Jose Roberto Andres <jandres@trinity.edu>
2018-06-22 21:12:46 -0500

    Fixed Corr

--- CorrMatrix.py
+++ CorrMatrix.py
@@ -7,10 +7,16 @@ import matplotlib.pyplot as plt
 
 file = 'Phish_Dataset.csv'
 df = pd.read_csv(file)
+add_df = df.set_index('id')
+add_df = add_df[['senders_email_tld']].applymap(lambda x: x.split('.')[0])
+df = df.drop(['senders_email_tld'], axis=1)
+df = df.set_index('id').join(add_df)
 
-dfY = df[df['target_phishYN']=="Y"].drop(['id'], axis=1)
-dfN = df[df['target_phishYN']=="N"].drop(['id'], axis=1)
+dfY = df[df['target_phishYN']=="Y"]
+dfN = df[df['target_phishYN']=="N"]
 
 corrY = dfY.corr()
 corrN = dfN.corr()
 
+print corrY
+print corrN
